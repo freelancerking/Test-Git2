c1483f96768141e1f4a51a5f1fa48e56378db5a7
freelancerking <54991778+freelancerking@users.noreply.github.com>
2021-07-03 23:13:56 +0700

    Add files via upload

---
+++ gamls.py
@@ -0,0 +1,54 @@
+import pprint
+import time
+import sqlite3
+import requests
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.proxy import Proxy, ProxyType
+
+
+payload = 'username=OLIVERCRYSTE&password=DreamTeam18!&sendLogin='
+s = requests.session()
+s.get("https://www.gamls.com")
+resp = s.post("https://www.gamls.com/login/login", data=payload)
+html = resp.text
+f = open("login.html", "w")
+f.write(html)
+f.close()
+
+
+'''WAIT = 5
+driver = webdriver.Chrome()
+driver.maximize_window()
+driver.get("https://www.gamls.com")
+time.sleep(WAIT)
+driver.find_element_by_id('username').send_keys("OLIVERCRYSTE")
+driver.find_element_by_id('password').send_keys("DreamTeam18!")
+driver.find_element_by_id('sendLoginLg').click()
+time.sleep(WAIT*2)
+'''
+
+
+'''with sync_playwright() as p:
+    for browser_type in [p.chromium]:
+        browser = browser_type.launch(headless=False)
+        page = browser.new_page()
+        page.set_default_timeout(120000)
+        page.goto('https://www.gamls.com/secure/default')
+        #input("solve captcha and enter to continue")
+        page.fill("#username", "OLIVERCRYSTE")
+        page.fill("#password", "DreamTeam18!")
+        page.click("#sendLoginLg")
+        time.sleep(WAIT*2)
+        page = 1
+        page.goto(f"https://www.gamls.com/secure/membership/index.cfm?Typ=Member&Fn=Name&Page={page}")
+        for tr in page.query_selector_all("tr"):
+            name = tr.query_selector_all("td")[0].inner_text()
+            company = tr.query_selector_all("td")[6].inner_text()
+            office = tr.query_selector_all("td")[7].inner_text()
+            cell = tr.query_selector_all("td")[8].inner_text()
+            email = tr.query_selector_all("td")[9].inner_text()
+            print(name,company,office,cell,email)
+
+
+'''
